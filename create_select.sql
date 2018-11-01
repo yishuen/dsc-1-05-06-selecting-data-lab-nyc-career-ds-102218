399229cd70e4ccaff6b8652f2ba6b515d15f1e78
yishuen <limyishuen@gmail.com>
2018-11-01 10:39:03 -0400

    added sql code to files

--- create_select.sql
+++ create_select.sql
@@ -1,1 +1,7 @@
-CREATE TABLE planets (id INTEGER PRIMARY KEY, name TEXT, color TEXT, num_of_moons INTEGER, mass REAL, rings REAL);
+CREATE TABLE planets (
+id INTEGER PRIMARY KEY, 
+name TEXT, 
+color TEXT, 
+num_of_moons INTEGER, 
+mass REAL, 
+rings REAL);
